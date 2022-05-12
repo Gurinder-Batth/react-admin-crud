287c8e8739f6027374d88af316c1d4969ca7861a
Gurinder Batth <gurinderbatth@192.168.1.8>
2022-05-13 00:05:42 +0530

    submission handler react admin

---
+++ client/src/components/FieldContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Fields from './Form/Fields'
+
+export default function FieldContainer() {
+  return (
+    <div>
+        <h1>Create Post Container 2 Level</h1>
+        <Fields />
+    </div>
+  )
+}
--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -1,80 +1,55 @@
-import React from 'react'
-import { Create, SimpleForm, TextInput, DateInput, BooleanInput } from 'react-admin'
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import React, { useState } from "react";
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  BooleanInput,
+  Toolbar,
+  SaveButton,
+  ShowButton,
+} from "react-admin";
+import Button from "@mui/material/Button";
 
+import Fields from "./Form/Fields";
+import SaveBtn from "./Btn/SaveBtn";
+import FieldContainer from "./FieldContainer";
+
+export const FormContext = React.createContext();
 
 const PostCreate = (props) => {
+  const [validationStatus, setValidationStatus] = React.useState(true);
 
-  const [open, setOpen] = React.useState(false);
-  const [publishState, setPublishState] = React.useState(true);
-
-
-  const handleClickOpen = () => {
-    setOpen(true);
+  const changeValidationStatus = (flag) => {
+    setValidationStatus(flag);
   };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const transformUser = (data) => ({
+    ...data,
+    fullbody: `${data.title} ${data.body}`,
+  });
 
-  const agree = () => {
-    setPublishState( (prev) => { return !prev});
-    setOpen(false);
+  const validateUserCreation = (values) => {
+    const errors = {};
+    // errors.firstName = '';
+    return errors;
   };
-
-  const changePublish = () => {
-    return publishState
-  }
 
   return (
     <div>
       <div>
-
-    <Create title='Create a Post' {...props}>
-      <SimpleForm>
-        <TextInput source='title' />
-        <TextInput multiline source='body' />
-        <DateInput label='Published' source='publishedAt' />
-        <BooleanInput label="Publish" 
-         onClick={handleClickOpen}
-         defaultValue={setPublishState}
-        //  parse={changePublish}
-         format={changePublish}
-         source="publish" />
-      </SimpleForm>
-    </Create>
-
-      
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={agree} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <FormContext.Provider
+          value={[validationStatus, changeValidationStatus]}
+        >
+          <Create title="Create a Post" {...props}>
+            <SimpleForm validate={validateUserCreation} toolbar={<SaveBtn />}>
+              <FieldContainer />
+            </SimpleForm>
+          </Create>
+        </FormContext.Provider>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostCreate
+export default PostCreate;
